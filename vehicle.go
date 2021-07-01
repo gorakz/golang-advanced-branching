5c4352fa573bf33b4ba9be197db575264bda5c24
rakzroamerz <rajeshgeek@gmail.com>
2021-07-01 01:11:20 -0500

    added additional vars

--- vehicle.go
+++ vehicle.go
@@ -14,11 +14,30 @@ type truck struct {
 	make        string
 	typeVehicle string
 }
-type bike struct{}
+type bike struct {
+	model string
+	make  string
+}
 
 // Values array for the feedback.json file
+type Values struct {
+	Models []Model `json:"values"`
+}
 
 // Model array for the feedback.json file
+type Model struct {
+	Name     string   `json:"model"`
+	Feedback []string `json:"feedback"`
+}
+
+type feedbackResult struct {
+	feedbacktotal    int
+	feedbackpositive int
+	feedbacknegative int
+}
+
+var vehicleResult map[string]feedbackResult
+var inventory []vehicle
 
 type rating float32
 
@@ -32,21 +51,20 @@ const (
 
 func init() {
 
-	/*
-		inventory = []vehicle{
-			bike{"FTR 1200", "Indian"},
-			bike{"Iron 1200", "Harley"},
-			car{"Sonata", "Hyundai", "Sedan"},
-			car{"SantaFe", "Hyundai", "SUV"},
-			car{"Civic", "Honda", "Hatchback"},
-			car{"A5", "Audi", "Coupe"},
-			car{"Mazda6", "Mazda", "Sedan"},
-			car{"CRV", "Honda", "SUV"},
-			car{"Camry", "Toyota", "Sedan"},
-			truck{"F-150", "Ford", "Truck"},
-			truck{"RAM1500", "Dodge", "Truck"}}
-	*/
-	//	vehicleResult = make(map[string]feedbackResult)
+	inventory = []vehicle{
+		bike{"FTR 1200", "Indian"},
+		bike{"Iron 1200", "Harley"},
+		car{"Sonata", "Hyundai", "Sedan"},
+		car{"SantaFe", "Hyundai", "SUV"},
+		car{"Civic", "Honda", "Hatchback"},
+		car{"A5", "Audi", "Coupe"},
+		car{"Mazda6", "Mazda", "Sedan"},
+		car{"CRV", "Honda", "SUV"},
+		car{"Camry", "Toyota", "Sedan"},
+		truck{"F-150", "Ford", "Truck"},
+		truck{"RAM1500", "Dodge", "Truck"}}
+
+	vehicleResult = make(map[string]feedbackResult)
 
 }
 
